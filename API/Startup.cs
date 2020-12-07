e5152e093d3ed95cb0133fcf6a2316ea209a5487
Vishnu Gupta <vishnu.gupta01@nagarro.com>
2020-12-07 12:33:16 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,6 +49,7 @@ namespace API
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseRouting();
 
