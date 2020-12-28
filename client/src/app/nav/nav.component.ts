7d410e816cd12fc2ff6402b5bcd0bd2336d9330f
Vishnu0522 <vishnugupta.bcs@gmail.com>
2020-12-28 17:02:28 +0530

    End of Section 6 (client side code).

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model:any={};
   IsLoggedIn =false;
-  constructor(private _accountService:AccountService) { }
+  constructor(public _accountService:AccountService,private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,13 +21,16 @@ export class NavComponent implements OnInit {
       this.model = user;
       console.log(this.model);
       this.IsLoggedIn = true;
+      this.toastr.success("User has logged In Successfully !!","sucess")
     },error =>{
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout(){
     this.IsLoggedIn = false;
+    this.toastr.success("User has logged out Successfully !!","sucess")
   }
 
 }
